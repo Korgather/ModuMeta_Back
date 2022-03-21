c661e46b6c8d28d898c7c92e7e3ef2c85d57903b
Muice <scm1400@gmail.com>
2022-03-21 15:37:36 +0900

    clone

---
+++ src/test/java/com/metaverse/station/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.station.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
