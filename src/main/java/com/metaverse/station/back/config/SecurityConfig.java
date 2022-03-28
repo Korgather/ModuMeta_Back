5c73795a198b3a8737094a281e282f41960210e6
Muice <scm1400@gmail.com>
2022-03-29 00:10:20 +0900

    frameoption 추가

--- src/main/java/com/metaverse/station/back/config/SecurityConfig.java
+++ src/main/java/com/metaverse/station/back/config/SecurityConfig.java
@@ -56,6 +56,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                     .cors()
                 .and()
+                    .headers().frameOptions().disable()
+                .and()
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
