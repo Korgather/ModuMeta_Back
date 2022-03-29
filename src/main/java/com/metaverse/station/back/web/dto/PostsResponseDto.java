032bd47fb33e0b0074d0d4e75682757d7fda3eab
Muice <scm1400@gmail.com>
2022-03-29 23:00:07 +0900

    컴파일 오류 수정

--- src/main/java/com/metaverse/station/back/web/dto/PostsResponseDto.java
+++ src/main/java/com/metaverse/station/back/web/dto/PostsResponseDto.java
@@ -9,14 +9,12 @@ public class PostsResponseDto {
     private Long id;
     private String title;
     private String content;
-    private String author;
     private String link;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
-        this.author = entity.getAuthor();
         this.link = entity.getLink();
 
     }
