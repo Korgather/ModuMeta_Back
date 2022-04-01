c21c62adf148177de71a0774891588c0f8143929
Muice <scm1400@gmail.com>
2022-04-02 00:08:01 +0900

    오류 수정

--- src/main/java/com/metaverse/station/back/domain/posts/PostsRepository.java
+++ src/main/java/com/metaverse/station/back/domain/posts/PostsRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface PostsRepository extends PagingAndSortingRepository<Posts,Long> {
 
 //    Page<Posts> findByUserOrderByIdDesc(User user, Pageable pageable);
-//    Page<Posts> findAll(Pageable pageable);
+    Page<Posts> findAll(Pageable pageable);
 
+    @Override
+    List<Posts> findAll();
 }
