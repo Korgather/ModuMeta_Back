4443df42fb0d2eeb93aa6eb109b18bef9ffa2fc2
Muice <scm1400@gmail.com>
2022-04-13 15:05:14 +0900

    어노테이션 유효성검사 예외 처리 클래스 추가

--- src/main/java/com/metaverse/station/back/web/exception/ErrorHandler.java
+++ src/main/java/com/metaverse/station/back/web/exception/ErrorHandler.java
@@ -1,14 +1,24 @@
 package com.metaverse.station.back.web.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.metaverse.station.back.web.dto.PostsSaveRequestDto;
+import com.nimbusds.oauth2.sdk.ErrorResponse;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.security.access.AccessDeniedException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -35,4 +45,13 @@ public class ErrorHandler {
                 "로그인후 이용해주세요", new HttpHeaders(),HttpStatus.FORBIDDEN
         );
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        ValidationErrorResponse errorResponse = ValidationErrorResponse.makeErrorResponse(e.getBindingResult());
+
+        return new ResponseEntity<ValidationErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
