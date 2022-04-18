75ea66d88cc73e50578671ec79438a0c7bb26807
Muice <scm1400@gmail.com>
2022-04-18 15:02:10 +0900

    스케줄러에 사용할 쿼리 작성

--- src/main/java/com/metaverse/station/back/domain/images/ImagesRepository.java
+++ src/main/java/com/metaverse/station/back/domain/images/ImagesRepository.java
@@ -1,7 +1,13 @@
 package com.metaverse.station.back.domain.images;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ImagesRepository extends JpaRepository<Images, Long> {
+
+    @Query("select l.imagePath from Images l")
+    List<String> getImage_pathList();
 
 }
