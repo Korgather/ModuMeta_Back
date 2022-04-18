80f02c96b79703f9f130aa54f47bcec0d7b3fd35
Muice <scm1400@gmail.com>
2022-04-19 03:21:37 +0900

    HomeController 추가

--- src/main/java/com/metaverse/station/back/web/HomeController.java
+++ src/main/java/com/metaverse/station/back/web/HomeController.java
@@ -1,2 +1,13 @@
-package com.metaverse.station.back.web;public class HomeController {
+package com.metaverse.station.back.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+
+        return "ModuMeta Rest Api Server";
+    }
 }
