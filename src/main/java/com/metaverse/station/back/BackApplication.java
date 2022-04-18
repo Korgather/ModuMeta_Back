1597b2cdbfbe070943f57acd793b932a3be2ae09
Muice <scm1400@gmail.com>
2022-04-18 15:01:45 +0900

    스케줄러 설정

--- src/main/java/com/metaverse/station/back/BackApplication.java
+++ src/main/java/com/metaverse/station/back/BackApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableConfigurationProperties({
         CorsProperties.class,
         AppProperties.class
 })
+@EnableScheduling
 @SpringBootApplication
 public class BackApplication {
 
