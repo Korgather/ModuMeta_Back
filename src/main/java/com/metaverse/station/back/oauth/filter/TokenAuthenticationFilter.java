7f18e42b112b404dc1563aa0c57c6d64a95bc3ca
Muice <scm1400@gmail.com>
2022-04-21 00:42:15 +0900

    토큰 인증이 필요하지 않은 api는 필터 제외

--- src/main/java/com/metaverse/station/back/oauth/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/metaverse/station/back/oauth/filter/TokenAuthenticationFilter.java
@@ -14,7 +14,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,7 +26,9 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request){
         String path = request.getServletPath();
-        return !path.startsWith("/api");
+        String method = request.getMethod();
+//        return !path.startsWith("/api");
+        return isResourceUrl(path,method);
     }
 
     @Override
@@ -47,6 +51,29 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
 
+    }
+
+    private boolean isResourceUrl(String url, String method) {
+        boolean isResourceUrl = false;
+
+        switch (method.toUpperCase()){
+            case "GET":
+                if(!url.startsWith("/api")){
+                    return true;
+                }
+                else{
+                    List<String> resourceRequests = Arrays.asList(
+                            "/api/v1/posts");
+                    for (String resourceRequest : resourceRequests) {
+                        if (url.contains(resourceRequest)) {
+                            isResourceUrl = true;
+                        }
+                    }
+                }
+                break;
+
+        }
+        return isResourceUrl;
     }
 
 }
