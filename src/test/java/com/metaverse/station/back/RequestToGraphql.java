d0625733697dfec2325b7c3b01e926f8a7b172f3
Muice <scm1400@gmail.com>
2022-04-23 22:10:01 +0900

    테스트

--- src/test/java/com/metaverse/station/back/RequestToGraphql.java
+++ src/test/java/com/metaverse/station/back/RequestToGraphql.java
@@ -17,84 +17,141 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.*;
 
 
-@SpringBootTest(
-        properties = {
-                "spring.config.location=classpath:application-test-test.properties"
-        }
-)
+//@SpringBootTest(
+//        properties = {
+//                "spring.config.location=classpath:application-test-test.properties"
+//        }
+//
+//)
 public class RequestToGraphql {
 
-    @MockBean
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    @Autowired
-    private PostsRepository postsRepository;
-
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    public void postToGraphql2() throws JsonProcessingException, JSONException {
-        String URL = "https://gt-space-data.herokuapp.com/graphql";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.add("content-type", "application/json"); // just modified graphql into json
-
-        List<Posts> list = postsRepository.findPostsByCategoryStringContaining("GATHERTOWN");
-        String query = "query gameData($apikey:String!,$spaceid:String!,$spacename:String!){gameData(spaceData:{apiKey: $apikey, spaceIdNum: $spaceid, spaceName: $spacename}){playerCount,}}";
-        String opertationName = "gameData";
-//        String spaceid = "At7RgsOoNxKkCFCY";
-//        String spacename = "il%20giardino%20di%20cassyboboh";
-//        String variables = "{\"apikey\": \"QUNCVEQGILsqeXR5\",\"spaceid\": \""+spaceid+"\",\"spacename\" : \""+spacename+"\"}";
-
-
-        list.forEach(posts -> {
-            String link = posts.getLink();
-            if(link.contains("gather.town")){
-                String spaceid = link.substring(link.indexOf("app/")+4,link.lastIndexOf("/"));
-                String spacename = link.substring(link.lastIndexOf("/")+1, link.length());
-                String variables = "{\"apikey\": \"QUNCVEQGILsqeXR5\",\"spaceid\": \""+spaceid+"\",\"spacename\" : \""+spacename+"\"}";
-
-                try {
-                    ResponseEntity<PlayerCountDto> response =
-                            restTemplate.postForEntity(URL, new HttpEntity<>(createJsonQueries(query,opertationName,variables), headers), PlayerCountDto.class);
-                    int playerCount = Objects.requireNonNull(response.getBody()).getData().getGameData().playerCount;
-                    posts.setPlayerCount(playerCount);
-                    System.out.println(playerCount);
-                } catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
-//                System.out.println("spaceid: "+ spaceid);
-//                System.out.println("spacename : " +spacename);
-
-            }
-            System.out.println(posts.getLink());
-        });
-
-
-
-//        ResponseEntity<PlayerCountDto> response = restTemplate.postForEntity(URL, new HttpEntity<>(createJsonQueries(query,opertationName,variables), headers), PlayerCountDto.class);
-//        System.out.println("The response================="+response);
-//        System.out.println(Objects.requireNonNull(response.getBody()).getData().getGameData().playerCount);
-    }
-
-    String createJsonQueries(String graphql, String operationName, String variables) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode wrapper = objectMapper.createObjectNode();
-        wrapper.put("query", graphql);
-        wrapper.put("operationName", operationName);
-
-        JsonNode jsonNode = objectMapper.readTree(variables);
-
-        wrapper.set("variables", jsonNode);
-        String queries = objectMapper.writeValueAsString(wrapper);
-
-        return queries;
-    }
+//    @MockBean
+//    private ClientRegistrationRepository clientRegistrationRepository;
+//
+//    @Autowired
+//    private PostsRepository postsRepository;
+//
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    public void postToGraphql2() throws JsonProcessingException, JSONException {
+//        String URL = "https://gt-space-data.herokuapp.com/graphql";
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//
+//
+//        headers.add("content-type", "application/json"); // just modified graphql into json
+//
+//        long startTime = System.currentTimeMillis();
+//
+//        List<Posts> list = postsRepository.findPostsByCategoryStringContaining("GATHERTOWN");
+//        String query = "query gameData($apikey:String!,$spaceid:String!,$spacename:String!){gameData(spaceData:{apiKey: $apikey, spaceIdNum: $spaceid, spaceName: $spacename}){playerCount,}}";
+//        String opertationName = "gameData";
+////        String spaceid = "At7RgsOoNxKkCFCY";
+////        String spacename = "il%20giardino%20di%20cassyboboh";
+////        String variables = "{\"apikey\": \"QUNCVEQGILsqeXR5\",\"spaceid\": \""+spaceid+"\",\"spacename\" : \""+spacename+"\"}";
+//
+//
+//        list.forEach(posts -> {
+//            String link = posts.getLink();
+//            if(link.contains("gather.town")){
+//                String spaceid = link.substring(link.indexOf("app/")+4,link.lastIndexOf("/"));
+//                String spacename = link.substring(link.lastIndexOf("/")+1, link.length());
+//                String variables = "{\"apikey\": \"QUNCVEQGILsqeXR5\",\"spaceid\": \""+spaceid+"\",\"spacename\" : \""+spacename+"\"}";
+//
+//                try {
+//                    ResponseEntity<PlayerCountDto> response =
+//                            restTemplate.postForEntity(URL, new HttpEntity<>(createJsonQueries(query,opertationName,variables), headers), PlayerCountDto.class);
+//                    int playerCount = Objects.requireNonNull(response.getBody()).getData().getGameData().playerCount;
+//                    posts.setPlayerCount(playerCount);
+//                    System.out.println(playerCount);
+//                } catch (JsonProcessingException e) {
+//                    e.printStackTrace();
+//                }
+////                System.out.println("spaceid: "+ spaceid);
+////                System.out.println("spacename : " +spacename);
+//
+//            }
+//            System.out.println(posts.getLink());
+//        });
+//
+//        long stopTime = System.currentTimeMillis();
+//        long elapsedTime = stopTime - startTime;
+//
+//        System.out.println(elapsedTime);
+//
+////        ResponseEntity<PlayerCountDto> response = restTemplate.postForEntity(URL, new HttpEntity<>(createJsonQueries(query,opertationName,variables), headers), PlayerCountDto.class);
+////        System.out.println("The response================="+response);
+////        System.out.println(Objects.requireNonNull(response.getBody()).getData().getGameData().playerCount);
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    public void postToGraphql3() throws JsonProcessingException, JSONException {
+//        String URL = "https://gt-space-data.herokuapp.com/graphql";
+//
+//        HttpHeaders headers = new HttpHeaders();
+//
+//        WebClient webClient = WebClient.builder().baseUrl(URL).defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+//
+//        headers.add("content-type", "application/json"); // just modified graphql into json
+//
+//        long startTime = System.currentTimeMillis();
+//
+//        List<Posts> list = postsRepository.findPostsByCategoryStringContaining("GATHERTOWN");
+//        String query = "query gameData($apikey:String!,$spaceid:String!,$spacename:String!){gameData(spaceData:{apiKey: $apikey, spaceIdNum: $spaceid, spaceName: $spacename}){playerCount,}}";
+//        String opertationName = "gameData";
+//
+//
+//        list.forEach(posts -> {
+//            String link = posts.getLink();
+//            if(link.contains("gather.town")){
+//                String spaceid = link.substring(link.indexOf("app/")+4,link.lastIndexOf("/"));
+//                String spacename = link.substring(link.lastIndexOf("/")+1, link.length());
+//                String variables = "{\"apikey\": \"QUNCVEQGILsqeXR5\",\"spaceid\": \""+spaceid+"\",\"spacename\" : \""+spacename+"\"}";
+//
+//                try {
+//                    String graphqlQuery = createJsonQueries(query,opertationName,variables);
+//                    Mono<PlayerCountDto> playerCountDto = webClient.post().bodyValue(graphqlQuery).retrieve().bodyToMono(PlayerCountDto.class);
+//
+//                } catch (JsonProcessingException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+////            System.out.println(posts.getLink());
+//        });
+//
+//        long stopTime = System.currentTimeMillis();
+//        long elapsedTime = stopTime - startTime;
+//
+//        System.out.println(elapsedTime);
+//
+////        ResponseEntity<PlayerCountDto> response = restTemplate.postForEntity(URL, new HttpEntity<>(createJsonQueries(query,opertationName,variables), headers), PlayerCountDto.class);
+////        System.out.println("The response================="+response);
+////        System.out.println(Objects.requireNonNull(response.getBody()).getData().getGameData().playerCount);
+//    }
+//
+//    String createJsonQueries(String graphql, String operationName, String variables) throws JsonProcessingException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectNode wrapper = objectMapper.createObjectNode();
+//        wrapper.put("query", graphql);
+//        wrapper.put("operationName", operationName);
+//
+//        JsonNode jsonNode = objectMapper.readTree(variables);
+//
+//        wrapper.set("variables", jsonNode);
+//        String queries = objectMapper.writeValueAsString(wrapper);
+//
+//        return queries;
+//    }
 
 }
