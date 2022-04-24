eceeb8535bfe041dd01d15e3644a0e042be5e419
Muice <scm1400@gmail.com>
2022-04-24 14:06:58 +0900

    @Transactional 어노테이션 추가, log() 제거

--- src/main/java/com/metaverse/station/back/utils/GetPlayerCountScheduler.java
+++ src/main/java/com/metaverse/station/back/utils/GetPlayerCountScheduler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +24,7 @@ import java.util.List;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class GetPlayerCountScheduler {
 
     private final WebClient.Builder webClientBuild;
@@ -65,7 +67,7 @@ public class GetPlayerCountScheduler {
 //                        System.out.println(playerCountDto.getData().gameData.getPlayerCount());
                         posts.setPlayerCount(playerCountDto.getData().gameData.getPlayerCount());
                     }
-            ).log().subscribe();
+            ).subscribe();
         });
 
 
