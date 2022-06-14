5d963bfea3386bb3e39c33db7981def3a1173ae7
Muice <scm1400@gmail.com>
2022-06-14 13:58:12 +0900

    오목유저 업데이트 Dto 클래스 추가

---
+++ src/main/java/com/metaverse/station/back/domain/gameRoom/omok/OmokUserUpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.metaverse.station.back.domain.gameRoom.omok;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+public class OmokUserUpdateRequestDto {
+
+    private Long id;
+
+    @Pattern(regexp = "^[가-힣ㄱ-ㅎa-zA-Z0-9._-]{2,10}$", message = "닉네임은 2글자 이상 10글자 이하, '-', '_'를 제외한 특수문자 및 공백을 포함 할 수 없습니다.")
+    private String nickname;
+
+    private int win;
+
+    private int lose;
+}
