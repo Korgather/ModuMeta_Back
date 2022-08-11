68853f141669c5876e4069dbbf5a203998a64f22
EarlGray <scm1400@gmail.com>
2022-08-11 23:01:16 +0900

    테스트3

--- src/main/java/com/metaverse/station/back/domain/gameRoom/GameRoomRepository.java
+++ src/main/java/com/metaverse/station/back/domain/gameRoom/GameRoomRepository.java
@@ -1,9 +1,11 @@
 package com.metaverse.station.back.domain.gameRoom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GameRoomRepository extends JpaRepository<GameRoom, Long> {
 
     Optional<GameRoom> findByUrl(String url);
